0f301e8e78378b74753c97e6c0ad6620b4d0f330
Martin Sanchez <tinchofiuba@gmail.com>
2024-06-13 09:09:00 -0300

    1

---
+++ analisisPie.py
@@ -0,0 +1,211 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from funcionesPie import InicioTalon,norma,generacionLinea,medicionPerimetro,PolyAjuste
+
+def func(data, a, b, c):
+    x, y = data
+    return a*x + b*y + c
+
+#defino variables a utilizar
+lZmin=[]
+lZmax=[]
+lYmin=[]
+lYmax=[]
+xyz=["X","Y","Z"]
+tamañoDato=8
+tamañoLandmark=8
+indice=0
+#comienzo
+escaneo='archivos/FootProfile2.xyz'
+Landmarks='archivos/Landmark2.xyz'
+df0=np.round(pd.read_table(escaneo,skiprows=2,delim_whitespace=(True),names=xyz),1)
+dfLandmarks=np.round(pd.read_table(Landmarks,delim_whitespace=(True),names=xyz),1)
+#indexDrop=[0,3,4,6,8,10,13,15] #landmarks que me parecieron NO útiles
+#dfLandmarks=dfLandmarks.drop(indexDrop) #indices dropeados
+df=df0.copy()
+for v in xyz: #me fijo si hay valores negativos, si es así llevo todos los valores a positivos
+    minv=df[v].min()   
+    if minv<0:
+        df[v]=df[v]+np.abs(minv)  
+        dfLandmarks[v]=dfLandmarks[v]+np.abs(minv)
+    else:
+        df[v]=df[v]-minv
+        dfLandmarks[v]=dfLandmarks[v]-minv
+#------------------------------------------------------------------------------
+minx=df['X'].min()
+maxx=df['X'].max()
+miny=df['Y'].min()
+maxy=df['Y'].max()
+minz=df['Z'].min()
+maxz=df['Z'].max()
+#------------------------------------------------------------------------------
+AlturaMaxTalon=df[df['Y']==miny]['Z'].min().round(1) 
+xAlturaMaxTalon=df[(df['Z']==AlturaMaxTalon) & (df['Y']==miny)]['X'].mean()
+arrayAlturaTalon=[xAlturaMaxTalon,0,AlturaMaxTalon]#<--------------------array alturaTalon
+distanciaTobillo=np.abs(dfLandmarks.iloc[1]['X']-dfLandmarks.iloc[2]['X'])#<--------------------largo del ancho del tobillo
+LargoPie=maxy-miny
+AnchoPie=maxx-minx
+arrayEntradaPie=dfLandmarks.iloc[11]#<--------------------array entradaPie
+# arrayCentroTobillo=dfLandmarks.iloc[12]
+# distanciaEntrada_Talon=norma(arrayEntradaPie,arrayAlturaTalon)#<--------------------distancia entre la entrada del pie y el talón
+# yInicioTalon,zInicioTalon=InicioTalon(df,lZmin,lYmin,AlturaMaxTalon,arrayCentroTobillo['Y'])
+# arrayInicioTalon=[xAlturaMaxTalon,yInicioTalon,zInicioTalon]#<--------------------array arrayInicioTalon
+# distanciaEntrada_IniciTalon=norma(arrayEntradaPie,arrayInicioTalon)#<--------------------distancia entre el empeine y el inicio del talón
+# print(f'Distancia Entrada de pie - inicio talon: {distanciaEntrada_IniciTalon}mm')
+# print(f'Ancho de tobillo: {distanciaTobillo}mm')
+# print(f'Distancia Entrada de pie - talon: {distanciaEntrada_Talon}mm')
+# print(f'Largo del pie: {LargoPie}mm')
+# print(f'Ancho del pie: {AnchoPie}mm')
+# ###############################################################
+# #--------------------------------------------------------------
+df['TIPO']='DATO'
+df['TAMAÑO']=tamañoDato
+# #--------------------------------------------------------------
+# dfInicioTalon=pd.DataFrame([arrayInicioTalon],columns=xyz)
+# dfInicioTalon['TIPO']='INICIO TALON'
+# dfInicioTalon['TAMAÑO']=tamañoLandmark
+# df=pd.concat([df,dfInicioTalon],ignore_index=True)
+# #--------------------------------------------------------------
+# dfEntradaPie=pd.DataFrame([arrayEntradaPie],columns=xyz)
+# dfEntradaPie['TIPO']='ENTRADA PIE'
+# dfEntradaPie['TAMAÑO']=tamañoLandmark
+# df=pd.concat([df,dfEntradaPie],ignore_index=True)
+# #--------------------------------------------------------------
+# dfAlturaTalon=pd.DataFrame([arrayAlturaTalon],columns=xyz)
+# dfAlturaTalon['TIPO']='ALTURA TALON'
+# dfAlturaTalon['TAMAÑO']=tamañoLandmark
+# df=pd.concat([df,dfAlturaTalon],ignore_index=True)
+# ###############################################################
+# ordIni=arrayInicioTalon[1]
+# absIni=arrayInicioTalon[2]
+# ordFin=arrayEntradaPie[1]
+# absFin=arrayEntradaPie[2]
+# #selecciono las columnas xyz y especifico el avance, la imagen del avance y los puntos de inicio y fin
+# intersecc=generacionLinea(df[xyz],'Y','Z',ordIni,ordFin,absIni,absFin)
+# intersecc['TIPO']='INICIO TALON - ENTRADA'
+# intersecc['TAMAÑO']=tamañoLandmark
+# df=pd.concat([df,intersecc],ignore_index=True)
+
+# ordIni=arrayAlturaTalon[1]
+# absIni=arrayAlturaTalon[2]
+# intersecc=generacionLinea(df[xyz],'Y','Z',ordIni,ordFin,absIni,absFin)
+# intersecc['TIPO']='TALON - ENTRADA'
+# intersecc['TAMAÑO']=tamañoLandmark
+# df=pd.concat([df,intersecc],ignore_index=True)
+# ordIni=arrayEntradaPie[1]
+# absIni=0
+# ordFin=ordIni
+# absFin=arrayEntradaPie[2]
+# #selecciono las columnas xyz y especifico el avance, la imagen del avance y los puntos de inicio y fin
+# intersecc,dictlargos=generacionLinea(df[xyz],'Z','Y',ordIni,ordFin,absIni,absFin)
+# df,dictlargos=medicionPerimetro(df,dictlargos,tamañoLandmark)
+# #fig=px.scatter_3d(df,x='X',y='Y',z='Z',color='TIPO',size='TAMAÑO',size_max=13)
+df=df.round(1)
+PolyAjuste(df,5,arrayEntradaPie,plano='XZ') #'H' para horizontal 'V' para vertical y 'D' para diagonal
+
+dfAprox=df[(df['Y']>arrayEntradaPie[1]-2) &(df['Y']<arrayEntradaPie[1]+2)]
+dfAprox['Y']=arrayEntradaPie[1]
+dfAprox['TIPO']='APROX'
+dfAprox['TAMAÑO']=tamañoDato
+#como el dataframe genera datos similares a los de una elipse divido el dataframe en 2 pedazos
+#el dataFrame que tiene valores de 'X' mayores a arrayEntradaPie[0] 
+#y el dataFrame que tiene valores de 'X' menores a arrayEntradaPie[0]
+df1=dfAprox[(dfAprox['X']>=arrayEntradaPie[0])]
+df2=dfAprox[(dfAprox['X']<arrayEntradaPie[0])]
+#encuentro en df1 el valore de Z maximo para un X maximo
+xMaxdf1=df1['X'].max()
+zMaxdf1=df1[df1['X']==xMaxdf1]['Z'].max()
+# separo al df1 y genero df1Sup y df1Inf segùn si es mayor o menor que zMaxdf1
+df1Sup=df1[df1['Z']>=zMaxdf1]
+df1Inf=df1[df1['Z']<zMaxdf1]
+#encuentro en df2 el valore de Z maximo para un X minimo
+xMindf2=df2['X'].min()
+zMaxdf2=df2[df2['X']==xMindf2]['Z'].max()
+# separo al df2 y genero df2Sup y df2Inf segùn si es mayor o menor que zMaxdf2
+df2Sup=df2[df2['Z']>=zMaxdf2]
+df2Inf=df2[df2['Z']<zMaxdf2]
+
+#ajusto con un polinomio de grado 4 los datos de df1Sup, df1Inf, df2Sup y df2Inf
+x1Sup=df1Sup['X']
+Z1Sup=df1Sup['Z']
+coeficientes1=np.polyfit(x1Sup,Z1Sup,5)
+p1Sup=np.poly1d(coeficientes1)
+#creo un vector X1SupAjustado que vaya desde x1Sup.min() hasta x1Sup.max() con un paso de 0.1
+X1SupAjustado=np.arange(x1Sup.min(),x1Sup.max(),0.1)
+Z1SupAjustado=p1Sup(X1SupAjustado)
+
+
+print(len(df1Sup))
+
+
+'''
+zmaxdfFinal=dfFinal['Z'].max()
+dfFinal1C=dfFinal[(dfFinal['X']>=arrayEntradaPie[0])]
+dfFinal2C=dfFinal[(dfFinal['X']<arrayEntradaPie[0])]
+xmaxdfFinal1C=dfFinal1C['X'].max()
+xmindfFinal2C=dfFinal2C['X'].min()
+print(xmindfFinal2C)
+zMaxdfFinal1Csup=dfFinal1C[dfFinal1C['X']==xmaxdfFinal1C]['Z'].max()
+zMaxdfFinal2Csup=dfFinal2C[dfFinal2C['X']==xmindfFinal2C]['Z'].max()
+dfFinal1Csup=dfFinal1C[dfFinal1C['Z']>=zMaxdfFinal1Csup]
+dfFinal2Csup=dfFinal2C[dfFinal2C['Z']>=zMaxdfFinal2Csup]
+print(dfFinal2Csup)
+#ajusto con un polinomio de grado 4 los datos de dfFinal1Csup
+x1Cs=dfFinal1Csup['X']
+Z1Cs=dfFinal1Csup['Z']
+coeficientes1=np.polyfit(x1Cs,Z1Cs,5)
+p1Cs=np.poly1d(coeficientes1)
+#creo un vector X1 que vaya desde x.min() hasta x.max() con un paso de 0.1
+X1CsAjustado=np.arange(x1Cs.min(),x1Cs.max(),0.1)
+Z1CsAjustado=p1Cs(X1CsAjustado)
+
+#creo un nuevo dfFinal1C con los valores de X1,Z1 y arrayEntradaPie[1]
+dfFinal1CsupAjustado=pd.DataFrame(columns=xyz)
+for i in range(len(X1CsAjustado)):
+    vector=[X1CsAjustado[i],arrayEntradaPie[1],Z1CsAjustado[i]]
+    dfFinal1CsupAjustado=dfFinal1CsupAjustado.append(pd.Series(vector,index=xyz),ignore_index=True)
+
+dfFinal1CsupAjustado['TIPO']='AJUSTADO1C'
+dfFinal1CsupAjustado['TAMAÑO']=tamañoDato
+
+x2Cs=dfFinal2Csup['X']
+print(x2Cs)
+Z2Cs=dfFinal2Csup['Z']
+coeficientes2=np.polyfit(x2Cs,Z2Cs,5)
+p2Cs=np.poly1d(coeficientes2)
+#creo un vector X1 que vaya desde x.min() hasta x.max() con un paso de 0.1
+X2CsAjustado=np.arange(x1Cs.min(),x1Cs.max(),0.1)
+Z2CsAjustado=p2Cs(X1CsAjustado)
+
+#creo un nuevo dfFinal1C con los valores de X1,Z1 y arrayEntradaPie[1]
+dfFinal2CsupAjustado=pd.DataFrame(columns=xyz)
+for i in range(len(X2CsAjustado)):
+    vector=[X2CsAjustado[i],arrayEntradaPie[1],Z2CsAjustado[i]]
+    dfFinal2CsupAjustado=dfFinal2CsupAjustado.append(pd.Series(vector,index=xyz),ignore_index=True)
+
+dfFinal2CsupAjustado['TIPO']='AJUSTADO2C'
+dfFinal2CsupAjustado['TAMAÑO']=tamañoDato
+#concateno dfFinal1CsupAjustado con dfFinal1Csup
+dfFinal1Csup=pd.concat([dfFinal1Csup,dfFinal1CsupAjustado],ignore_index=True)
+dfFinal2Csup=pd.concat([dfFinal2Csup,dfFinal2CsupAjustado],ignore_index=True)   
+'''
+#dfPromedio=dfPromedio.append(dfFinala[(dfFinala['Y']>=i)&(dfFinala['Y']<i+10)].mean(),ignore_index=True)
+#dfFinalb=dfFinal[dfFinal['X']>arrayEntradaPie[0]]
+#df=pd.concat([df,dfPromedio],ignore_index=True)
+dfjuntado=pd.concat([df1Sup,df1Inf],ignore_index=True)
+fig=px.scatter_3d(dfjuntado,x='X',y='Y',z='Z',color='TIPO',size='TAMAÑO',size_max=13)
+# for key, dfcurva in dictlargos.items():
+#     fig.add_trace(go.Scatter3d(x=dfcurva['X'], y=dfcurva['Y'], z=dfcurva['Z'], mode='lines', name=key,line=dict(color='red', width=4)))
+# for i in range(2):
+#     fig.add_trace(go.Scatter3d(x=dictlargos['PMaxm']['X'], y=dictlargos['PMaxm']['Y'], z=dictlargos['PMaxm']['Z'], mode='lines', name='PMaxm',line=dict(color='red', width=4)))
+#     fig.add_trace(go.Scatter3d(x=dictlargos['PMaxM']['X'], y=dictlargos['PMaxM']['Y'], z=dictlargos['PMaxM']['Z'], mode='lines', name='PMaxM',line=dict(color='red', width=4)))
+fig.update_layout(scene=dict(aspectratio=dict(x=1.1, y=3.1, z=1),))
+fig.show()
+#fig.write_html("plot.html")
+
+
+
+
---
+++ funcionesPie.py
@@ -0,0 +1,177 @@
+import numpy as np
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def InicioTalon(df,lZmin,lYmin,AlturaMaxTalon,limiteY):
+    df_y=df[df['Y']<limiteY/2]
+    for y in np.round(df_y['Y'].unique(),1):
+        df_y2 = df_y[df_y['Y'] == y]
+        if df_y2['Z'].min() < AlturaMaxTalon:
+            lZmin.append(df_y2['Z'].min())
+            lYmin.append(y)
+    lZmin=lZmin[::-1]
+    largo=int(len(lZmin)/3) 
+    max_lZmin=np.max(lZmin[:largo])
+    for v in lZmin:
+        if v>max_lZmin:
+            indice=lZmin.index(v)
+            if lZmin[indice+2]>max_lZmin:
+                break
+    return lYmin[indice],lZmin[indice]
+
+def norma(v1, v2):
+    return np.linalg.norm(np.array(v1) - np.array(v2))
+
+def Maximos(listaMax):
+  maximos=[]
+  largos=[]
+  i=0
+  for i,v in enumerate(listaMax):
+    if i!=0:
+      if v>listaMax[i-1]:
+          if i+1<(len(listaMax)-1):
+              if v>=listaMax[i+1]:
+                  if v in maximos:
+                    print('repetido')
+                  else:
+                    maximos.append(v)
+                    largos.append(i)
+    else:
+      maximos.append(v)
+      largos.append(i)
+  return (maximos,largos)
+   
+def Minimos(listaMin):
+  i=0
+  largos=[]
+  minimos=[]
+  for i,v in enumerate(listaMin):
+    if i!=0:
+      if v<listaMin[i-1]:
+        if i+1<(len(listaMin)-1):
+          if v<listaMin[i+1]:
+              if v in minimos:
+                 print('repetido')
+              else:
+                minimos.append(v)
+                largos.append(i)
+    else:
+      minimos.append(v)
+      largos.append(i)
+  return (minimos,largos)
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+def PolyAjuste(df,orden,punto,**kwargs):
+   for key, value in kwargs.items():
+      print(f'{key} = {value}')
+ 
+
+def medicionPerimetro(df,dictlargos,tamañoLandmark):
+  j=0
+  dictPerimetroSumados={}
+  listaPerimetros=[]
+  for key,dfcurva in dictlargos.items():
+      dfcurva['TIPO']=key
+      dfcurva['TAMAÑO']=tamañoLandmark
+      dfcurva.sort_values(by='Z', ascending=False, inplace=True)
+      #teniendo las coordenadas x,y,z calculo el largo de la curva
+      largoCurva=0
+      largoCurvas=[]
+      for i in range(len(dfcurva)-1):
+          coordenada=dfcurva[df.columns[0:3]]
+          largoCurva+=norma(coordenada.iloc[i],coordenada.iloc[i+1])
+      dictPerimetroSumados['Largo '+key]=largoCurva
+      print(f'Largo de {key}: {largoCurva}mm')
+      j+=1
+  #itero de dos en dos para sumar key0 con key1 y asì sucesivamente
+  for i in range(0,len(dictPerimetroSumados),2):
+      key0=list(dictPerimetroSumados.keys())[i]
+      key1=list(dictPerimetroSumados.keys())[i+1]
+      suma=dictPerimetroSumados[key0]+dictPerimetroSumados[key1]
+      listaPerimetros.append(suma)
+  indiceMax=listaPerimetros.index(max(listaPerimetros))
+  dictlargos['PMaxm']=dictlargos[list(dictlargos.keys())[indiceMax*2]]
+  dictlargos['PMaxM']=dictlargos[list(dictlargos.keys())[indiceMax*2+1]]
+  perimMax=np.max(listaPerimetros)
+  print(f'El perimetro máximo es: {perimMax}mm')
+  df=pd.concat([df,dictlargos['PMaxm']],ignore_index=True)
+  df=pd.concat([df,dictlargos['PMaxM']],ignore_index=True)
+  return df,dictlargos 
+
+def curvasPerimetrales(df,ejeMax,ejeCorte): #ejemax, eje donde se busca un máximo, ejecorte,
+  #eje en el cual se buscan valores mayores o menores al corte
+  maxZ=np.round(df[ejeMax].max(),1)
+  df1=df.copy()
+  df1=df1[df1[ejeMax]==maxZ]
+  if len(df1)==1:
+    corte=df1[ejeCorte].values[0]
+    df1a=df[df[ejeCorte]>=corte] 
+    df1b=df[df[ejeCorte]<corte]
+  else:
+    corte=df1[ejeCorte].mean()
+    df1a=df[df[ejeCorte]>=corte] 
+    df1b=df[df[ejeCorte]<corte]
+  return df1a,df1b
+
+def caso_vertical(df,avance,avanceRecta,ultimaCol,ordIni):
+  dictlargos={}
+  #armo un dict vacio
+  dfm,dfM=curvasPerimetrales(df[df[avanceRecta]==ordIni],avance,ultimaCol)
+  #añado al diccionario las curvas
+  dictlargos['curvaLandmark_m']=dfm
+  dictlargos['curvaLandmark_M']=dfM
+  for paso in range(1,13,1):
+    dfm,dfM=curvasPerimetrales(df[df[avanceRecta]==ordIni-np.round(paso/10,1)],avance,ultimaCol)
+    dictlargos[f'curva+{paso}m']=dfm
+    dictlargos[f'curva+{paso}M']=dfM
+    dfm,dfM=curvasPerimetrales(df[df[avanceRecta]==ordIni-np.round(-paso/10,1)],avance,ultimaCol)
+    dictlargos[f'curva-{paso}m']=dfm
+    dictlargos[f'curva-{paso}M']=dfM
+  #maximo1=dfma[avanceRecta].max()
+  #dfmaxavancerecta=dfma[dfma[avanceRecta]==maximo1][avance]
+  return pd.DataFrame(df[df[avanceRecta]==ordIni]),dictlargos #devuelve df y luego las curvas.
+#las curvas van el orden de (curva1,curva2,y el resto en duplas que van en 4,8,12 +-
+
+def generacionLinea(df,avance,avanceRecta,ordIni,ordFin,abscisaIni,abscisaFin):
+    #en función de las coordenadas ingresadas en "avance" y en "avanceRecta"
+    #obtengo la variable que no es ni "avance" ni "avanceRecta"
+    col=df.columns
+    colDif=col.difference([avance,avanceRecta])
+    ultimaCol=colDif.to_list()[0]
+    columnas=[avance,avanceRecta,ultimaCol]
+    dictCurva={avance:[],avanceRecta:[],ultimaCol:[]}
+    #ingreso un df y los puntos inciales para obtener una linea y calculo m y b
+    if (ordFin-ordIni)==0: #si quiero un corte vertical
+      return caso_vertical(df,avance,avanceRecta,ultimaCol,ordIni)
+    elif (abscisaFin-abscisaIni)==0: #si quiero un corte horizontal (en este caso no serìa útil)
+      df1a,df2b=curvasPerimetrales(df[df[avanceRecta]==ordIni],avance,ultimaCol)
+      return pd.DataFrame(df[df[avanceRecta]==ordFin]),df1a,df2b
+    else:
+      m=round((abscisaFin-abscisaIni)/(ordFin-ordIni),6)
+      b=round(abscisaIni-ordIni*m,6)
+      #genero una linea con np.linspace con un paso de 0.1 entre ordIni y ordFin
+      NumPasos=int(abs(ordFin-ordIni)*10)
+      pasos=np.round(np.linspace(ordIni,ordFin,NumPasos),1)#hago los pasos en el sentido de las ordenadas
+      for paso in pasos:
+        PasoRecta=round(m*paso+b,1)
+        coord=df[(df[avance]==paso) & (df[avanceRecta]==PasoRecta)][ultimaCol]
+        if len(coord)>1:
+          dictCurva[columnas[0]].append(paso)
+          dictCurva[columnas[1]].append(PasoRecta)
+          dictCurva[columnas[2]].append(np.round(coord.min(),1))
+          dictCurva[columnas[0]].append(paso)
+          dictCurva[columnas[1]].append(PasoRecta)
+          dictCurva[columnas[2]].append(np.round(coord.max(),1))
+        elif len(coord)==1:
+          dictCurva[columnas[0]].append(paso)
+          dictCurva[columnas[1]].append(PasoRecta)
+          dictCurva[columnas[2]].append(np.round(coord.min(),1))
+      
+      return pd.DataFrame(dictCurva)
+
+    
+
+
